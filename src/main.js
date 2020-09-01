96e5d5181e9f4bbc7a81197866279758ab2e8580
abhishek sarmah <abhisheksarmah660@gmail.com>
2020-09-01 10:54:58 +0530

    netlify redirect and closable

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/css/main.css";
+import {
+  closable
+} from './directives';
 
 Vue.config.productionTip = false;
+Vue.directive('closable', closable)
 
 new Vue({
   router,
